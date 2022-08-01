d6e637fa96a956ffb9362835af17e896ae5dafab
Tatarik-2 <101043448+Tatarik-2@users.noreply.github.com>
2022-08-01 23:41:48 +0300

    Dobavil chtenie givotnyh iz faila

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileReader reader =
+                new FileReader("C:\\Users\\niy99\\Desktop\\Файлы со стола\\Programs\\животные.txt");
+        Scanner scanner = new Scanner(reader);
+        List<String> wordList = new ArrayList<>();
+        Pattern pattern =Pattern.compile("[А-я]+");
+
+        String allAnimalsToString=new String();
+        while (scanner.hasNext()){
+            allAnimalsToString+= scanner.nextLine();
+        }
+
+        Matcher matcher = pattern.matcher(allAnimalsToString);
+        while (matcher.find()){
+            wordList.add(matcher.group());
+        }
+        reader.close();
+
         boolean ready = true;
         while (ready) {
-            List<String> wordList = new ArrayList<>(Arrays.asList(
-                    "бегемотиха", "крокодильчик", "гипопатам", "антилопа", "шимпанзе", "шакалиха"
-            ));
+//            List<String> wordList = new ArrayList<>(Arrays.asList(
+//                    "бегемотиха", "крокодильчик", "гипопатам", "антилопа", "шимпанзе", "шакалиха"
+//            ));
+
             String word = wordList.get((int) (Math.random() * wordList.size()));
             char[] wordChars = word.toCharArray();
             char[] newChars = new char[wordChars.length];
@@ -35,14 +58,13 @@ public class Main {
             String answer = scanner2.next();
             if (answer.equals(word)) {
                 System.out.println("Поздравляем! Вы угадали!");
+                continue;
             } else {
                 System.out.println("К сожалению, неверно(((");
             }
             System.out.println("Сыграем еще раз? (да/нет)");
             Scanner scanner3 = new Scanner(System.in);
             switch (scanner3.next()) {
-
-
                 case "да":
                     break;
                 case "нет":
